9ef6bcf64b431cc0541e6b41f6859ade98234500
Bitzcanon <winston.ngoui@gmail.com>
2025-05-05 15:11:06 +0800

    Added extra info to the modal

--- js/custom.js
+++ js/custom.js
@@ -18,3 +18,11 @@ $(function()
     new WOW().init();
     $(".rotate").textrotator();
 })
+
+// tooltips
+const tooltipTriggerList = document.querySelectorAll(
+	'[data-bs-toggle="tooltip"]'
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+	(el) => new bootstrap.Tooltip(el)
+  );
